78cca4ede30cc846b8e3a94b9bfdefb68d9b041f
John Maddux <jemaddux@gmail.com>
2013-06-27 22:20:19 -0600

    FIRST

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SecondBookcase::Application.config.secret_key_base = '4cd94a7677fd0b7eb7f07f98c68a8c48564a836fdd88fd94f13e4724ea27dd5c734e085d8cb162bb09e0d929f9b61adfe163b27da93740071f66ba1bb33082c5'
