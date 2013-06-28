21713cb1cc15e938c68e26371b456c027cf2e72b
John Maddux <jemaddux@gmail.com>
2013-06-27 22:46:31 -0600

    Added author field to books

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130628042331) do
+ActiveRecord::Schema.define(version: 20130628044545) do
 
   create_table "books", force: true do |t|
     t.string   "name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20130628042331) do
     t.integer  "times_rated"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "author"
   end
 
 end
