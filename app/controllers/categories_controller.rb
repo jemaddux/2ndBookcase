f342d2bb0b3b9bc285ae7bd8af4ffa80608c727e
John Maddux <jemaddux@gmail.com>
2013-06-29 23:19:22 -0600

    Categories now have books

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -10,6 +10,7 @@ class CategoriesController < ApplicationController
   # GET /categories/1
   # GET /categories/1.json
   def show
+    @books = @category.books.page(params[:page]).per_page(50)
   end
 
   # GET /categories/new
