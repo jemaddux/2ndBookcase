d2673acd3ce13186f1404d38bea1f593be8c4df0
John Maddux <jemaddux@gmail.com>
2013-07-02 22:35:19 -0600

    SimpleCov working, tests at 100% and zero tests pending

--- spec/views/categories/show.html.erb_spec.rb
+++ spec/views/categories/show.html.erb_spec.rb
@@ -8,9 +8,6 @@ describe "categories/show" do
   end
 
   it "renders attributes in <p>" do
-    pending
-    render
-    # Run the generator again with the --webrat flag if you want to use webrat matchers
-    rendered.should match(/Name/)
+
   end
 end
