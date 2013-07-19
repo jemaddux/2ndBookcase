7e5c1b339ea73af721cede6214c95f8b12a3aaac
John Maddux <jemaddux@gmail.com>
2013-07-18 22:22:19 -0600

    Moved code from a controller to a model

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -10,41 +10,12 @@ class AdminsController < ApplicationController
   end
 
   def checkout_book
-    # inventory = first_available_inventory(params["book_id"])
-    # inventory.checked_out = true
-    # inventory.save
-
-    # reading_list = ReadingList.find(params["reading_list_id"])
-    # reading_list.loan_out_date = Date.today
-    # reading_list.loan_out_condition = inventory.condition
-    # reading_list.inventory_id = inventory.id
-    # reading_list.out_on_loan = true
-    # reading_list.save
     Book.checkout_book(params)
-
     redirect_to :back
   end
 
   def check_in_book
-    i = Inventory.find(params[:inventory_id])
-    i.checked_out = false
-    i.condition = params[:returned_condition]
-    i.in_circulation = false if params[:returned_condition] == "retired"
-    i.save
-
-    rl_dataset = ReadingList.where(out_on_loan: true)
-    rl = rl_dataset.find_by_inventory_id(params[:inventory_id])
-
-    rl.returned_date = Date.today
-    rl.out_on_loan = false
-    rl.in_list = false
-    if params[:returned_condition]
-      rl.returned_condition = params[:returned_condition]
-    else
-      rl.returned_condition = rl.loan_out_condition
-    end
-    rl.save
-
+    Book.check_in_book(params)
     redirect_to :back
   end
 
--- app/models/book.rb
+++ app/models/book.rb
@@ -19,6 +19,27 @@ class Book < ActiveRecord::Base
     reading_list.save
   end
 
+  def self.check_in_book(params)
+    i = Inventory.find(params[:inventory_id])
+    i.checked_out = false
+    i.condition = params[:returned_condition]
+    i.in_circulation = false if params[:returned_condition] == "retired"
+    i.save
+
+    rl_dataset = ReadingList.where(out_on_loan: true)
+    rl = rl_dataset.find_by_inventory_id(params[:inventory_id])
+
+    rl.returned_date = Date.today
+    rl.out_on_loan = false
+    rl.in_list = false
+    if params[:returned_condition]
+      rl.returned_condition = params[:returned_condition]
+    else
+      rl.returned_condition = rl.loan_out_condition
+    end
+    rl.save
+  end
+
 private
 
   def self.first_available_inventory(book_id)
