4f1c390774e225c94b455da67b681e07789ce035
John Maddux <jemaddux@gmail.com>
2013-10-28 22:52:19 -0400

    Put some of the controllers in a folder

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,8 @@ Bundler.require(:default, Rails.env)
 module SecondBookcase
   class Application < Rails::Application
     config.autoload_paths += %W(#{config.root}/app/models/bookshelf)
+    config.autoload_paths += %W(#{config.root}/app/controllers/bookshelf)
+    config.autoload_paths += %W(#{config.root}/app/views/bookshelf)
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
