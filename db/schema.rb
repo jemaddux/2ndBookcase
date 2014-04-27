310307810ecf962a3ade8f1ab2a0289a053f5a6e
John Maddux <jemaddux@gmail.com>
2014-04-27 18:26:27 -0400

    pg

--- db/schema.rb
+++ db/schema.rb
@@ -87,7 +87,6 @@ ActiveRecord::Schema.define(version: 20131030014455) do
     t.string   "state"
     t.string   "zipcode"
     t.string   "account_status"
-    t.string   "rental_plan"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "password_digest"
