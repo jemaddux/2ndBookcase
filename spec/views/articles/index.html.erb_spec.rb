f259dfcfc8ee6413c7d9edf23c19f0ae7959e923
John Maddux <jemaddux@gmail.com>
2014-05-10 12:47:01 -0400

    Rubocop

--- spec/views/articles/index.html.erb_spec.rb
+++ spec/views/articles/index.html.erb_spec.rb
@@ -1,21 +1,21 @@
 require 'spec_helper'
 
-describe "articles/index" do
+describe 'articles/index' do
   before(:each) do
     assign(:articles, [
       stub_model(Article,
-        :title => "Title",
-        :body => "MyText",
-        :customer_id => 1,
-        :rating => 1.5,
-        :published => false
+        title: 'Title',
+        body: 'MyText',
+        customer_id: 1,
+        rating: 1.5,
+        published: false
       ),
       stub_model(Article,
-        :title => "Title",
-        :body => "MyText",
-        :customer_id => 1,
-        :rating => 1.5,
-        :published => false
+        title: 'Title',
+        body: 'MyText',
+        customer_id: 1,
+        rating: 1.5,
+        published: false
       )
     ])
   end
--- spec/views/articles/show.html.erb_spec.rb
+++ spec/views/articles/show.html.erb_spec.rb
@@ -1,17 +1,17 @@
 require 'spec_helper'
 
-describe "articles/show" do
+describe 'articles/show' do
   before(:each) do
     @article = assign(:article, stub_model(Article,
-      :title => "Title",
-      :body => "MyText",
-      :customer_id => 1,
-      :rating => 1.5,
-      :published => false
+      title: 'Title',
+      body: 'MyText',
+      customer_id: 1,
+      rating: 1.5,
+      published: false
     ))
   end
 
-  it "renders attributes in <p>" do
+  it 'renders attributes in <p>' do
     render
     # Run the generator again with the --webrat flag if you want to use webrat matchers
     rendered.should match(/Title/)
